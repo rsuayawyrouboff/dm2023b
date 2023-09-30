587d6b573d1dd1ef2629d1075a7d6a9734ea9686
rsuayawyrouboff <rsuayawyrouboff@itba.edu.ar>
2023-09-30 12:14:50 +0000

    prueb desde vm

--- src/rpart/r0_211_traintest_estratificado.r
+++ src/rpart/r0_211_traintest_estratificado.r
@@ -5,7 +5,7 @@ require("data.table")
 require("rpart")
 
 PARAM <- list()
-PARAM$semilla <- 102191
+PARAM$semilla <- 125669
 
 #------------------------------------------------------------------------------
 # particionar agrega una columna llamada fold a un dataset
@@ -33,7 +33,7 @@ particionar <- function(
 
 # Aqui se debe poner la carpeta de la computadora local
 # Establezco el Working Directory
-setwd("X:\\gdrive\\ITBA2023B\\")
+setwd("~/buckets/b1")
 
 # cargo los datos
 dataset <- fread("./datasets/dataset_pequeno.csv")
