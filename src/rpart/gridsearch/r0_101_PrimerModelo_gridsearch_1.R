9555ac19ebe2052f10518943e4f586a67f720f3f
rsuayawyrouboff <rsuayawyrouboff@itba.edu.ar>
2023-10-01 09:41:31 -0300

    gridsearch

--- src/rpart/gridsearch/r0_101_PrimerModelo_gridsearch_1.R
+++ src/rpart/gridsearch/r0_101_PrimerModelo_gridsearch_1.R
@@ -60,6 +60,6 @@ dir.create("./exp/KA2002")
 
 # solo los campos para Kaggle
 fwrite(dapply[, list(numero_de_cliente, Predicted)],
-        file = "./exp/KA2001/K101_002_1.csv",
+        file = "./exp/KA2002/K101_002_1.csv",
         sep = ","
 )
