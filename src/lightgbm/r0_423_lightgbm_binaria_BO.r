5ced8400e3057031d7c7e1b886391586f6e852d3
rsuayawyrouboff <rsuayawyrouboff@itba.edu.ar>
2023-10-01 20:34:00 +0000

    z423hyperparameters

--- src/lightgbm/r0_423_lightgbm_binaria_BO.r
+++ src/lightgbm/r0_423_lightgbm_binaria_BO.r
@@ -57,6 +57,9 @@ PARAM$hyperparametertuning$semilla_azar <- 125683
 hs <- makeParamSet(
   makeNumericParam("learning_rate", lower = 0.01, upper = 0.3),
   makeNumericParam("feature_fraction", lower = 0.2, upper = 1.0),
+  makeNumericParam("lambda_l1", lower = 0.0001, upper = 1.0),
+  makeNumericParam("lambda_l2", lower = 0.0001, upper = 1.0),
+  makeNumericParam("min_gain_to_split", lower = 0, upper = 1.0),
   makeIntegerParam("min_data_in_leaf", lower = 1L, upper = 8000L),
   makeIntegerParam("num_leaves", lower = 16L, upper = 1024L),
   makeIntegerParam("envios", lower = 5000L, upper = 15000L)
@@ -143,9 +146,6 @@ EstimarGanancia_lightgbm <- function(x) {
     feature_pre_filter = FALSE,
     verbosity = -100,
     max_depth = -1, # -1 significa no limitar,  por ahora lo dejo fijo
-    min_gain_to_split = 0.0, # por ahora, lo dejo fijo
-    lambda_l1 = 0.0, # por ahora, lo dejo fijo
-    lambda_l2 = 0.0, # por ahora, lo dejo fijo
     max_bin = 31, # por ahora, lo dejo fijo
     num_iterations = 9999, # valor grande, lo limita early_stopping_rounds
     force_row_wise = TRUE, # para evitar warning
