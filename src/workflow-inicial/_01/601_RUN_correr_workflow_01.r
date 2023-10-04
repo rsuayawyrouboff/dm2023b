f0ddec1cb36fd39dff7117bb7fbf679f124baf9a
rsuayawyrouboff <rsuayawyrouboff@itba.edu.ar>
2023-10-03 23:59:43 -0300

    cleanup

--- src/workflow-inicial/_01/601_RUN_correr_workflow_01.r
+++ src/workflow-inicial/_01/601_RUN_correr_workflow_01.r
@@ -10,11 +10,11 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/dm2023b/src/workflow-inicial/611_CA_reparar_dataset_01.r")
-source("~/dm2023b/src/workflow-inicial/621_DR_corregir_drifting_01.r")
-source("~/dm2023b/src/workflow-inicial/631_FE_historia_01.r")
-source("~/dm2023b/src/workflow-inicial/641_TS_training_strategy_01.r")
+source("~/dm2023b/src/workflow-inicial/_01/611_CA_reparar_dataset_01.r")
+source("~/dm2023b/src/workflow-inicial/_01/621_DR_corregir_drifting_01.r")
+source("~/dm2023b/src/workflow-inicial/_01/631_FE_historia_01.r")
+source("~/dm2023b/src/workflow-inicial/_01/641_TS_training_strategy_01.r")
 
 # ultimos pasos, muy lentos
-source("~/dm2023b/src/workflow-inicial/651_HT_lightgbm_01.r")
-source("~/dm2023b/src/workflow-inicial/661_ZZ_final_01.r")
+source("~/dm2023b/src/workflow-inicial/_01/651_HT_lightgbm_01.r")
+source("~/dm2023b/src/workflow-inicial/_01/661_ZZ_final_01.r")
